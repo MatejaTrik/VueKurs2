7d9feca75720674c8fa50fc1f1bc6a9ae7e7daec
MatejaTrik <mtrikic2@gmail.com>
2020-04-27 20:53:34 +0200

    27.04.2020.

---
+++ domnoo/src/App.vue
@@ -0,0 +1,30 @@
+// eslint-disable-file no-use-before-define 
+
+
+<template>
+  <div>
+    <comp-header></comp-header>
+    <div>
+      content
+    </div>
+    <comp-footer></comp-footer>
+  </div>
+</template>
+
+<script>
+  import compHeader from './components/Header-Footer/Header'
+  import compFooter from './components/Header-Footer/Footer'
+
+export default {
+  components:{
+    compHeader,
+    compFooter,
+  }
+}
+</script>
+
+<style>
+
+  @import './assets/style.css';
+
+</style>
---
+++ domnoo/src/components/Header-Footer/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <div class="container clear">
+             <div class="logo">
+                <img :src="logoIcon">
+             </div>
+             <div class="nav">
+                <img @click.prevent="showSidepanel = !showSidepanel" :src="menuIcon">
+             </div>
+             <md-drawer 
+                class="md-right side-drawer"
+                :md-active.sync="showSidepanel"
+             />
+        </div>
+    </header>
+</template>
+
+<script>
+    import Logo from '../../assets/images/logo.png'
+    import Menu from '../../assets/images/menu.png'
+
+    export default {
+        data() {
+            return{
+                logoIcon: Logo,
+                menuIcon: Menu,
+                showSidepanel: false
+            }
+        }
+    }
+</script>
